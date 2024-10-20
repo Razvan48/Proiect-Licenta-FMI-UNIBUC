3853239b0b80bbc965443ff8b0453d52a87cae6b
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2024-10-21 00:34:48 +0300

    setup FreeType

--- BoardGameBattle/BoardGameBattle/BoardGameBattle.cpp
+++ BoardGameBattle/BoardGameBattle/BoardGameBattle.cpp
@@ -10,9 +10,26 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
+#include <ft2build.h>
+#include FT_FREETYPE_H  
+
 int main()
 {
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return -1;
+    }
 
+    /*
+    FT_Face face;
+    if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return -1;
+    }
+    */
 
 	return 0;
 }
