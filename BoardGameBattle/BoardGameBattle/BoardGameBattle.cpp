fb7668d0714c6783f501aa046c509ef5df380cdd
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2024-10-20 23:19:50 +0300

    fixed glfw3 external symbols unrecognized, needed to include user32.lib gdi32.lib shell32.lib

--- BoardGameBattle/BoardGameBattle/BoardGameBattle.cpp
+++ BoardGameBattle/BoardGameBattle/BoardGameBattle.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 int main()
 {
 
+
 	return 0;
 }
+
+
+
