7eeb92277f97ff3b2bc5fa6b00fdd3ad63538198
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2024-10-29 20:44:22 +0200

    blocked window resizing

--- BoardGameBattle/BoardGameBattle/source/WindowManager/WindowManager.cpp
+++ BoardGameBattle/BoardGameBattle/source/WindowManager/WindowManager.cpp
@@ -8,9 +8,10 @@ WindowManager::WindowManager()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Blocheaza redimensionarea ferestrei
 
     this->window = glfwCreateWindow(this->WINDOW_WIDTH, this->WINDOW_HEIGHT, this->WINDOW_TITLE.c_str(), 0, 0);
-    // glfwGetPrimaryMonitor();
+	// glfwGetPrimaryMonitor(); // pentru fullscreen
 
     glfwMakeContextCurrent(this->window);
 
