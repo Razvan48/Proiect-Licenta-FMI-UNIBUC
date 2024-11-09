2fb2cc3723cf6fd35998de845b7db18d5a9b84c1
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2024-11-10 00:46:46 +0200

    implemented server singleton to a certain extend, stable commit

--- BoardGameBattle/BoardGameBattle/source/Server/Server.cpp
+++ BoardGameBattle/BoardGameBattle/source/Server/Server.cpp
@@ -1,13 +1,19 @@
 #include "Server.h"
 
 #include "../RandomGenerator/RandomGenerator.h"
+#include "../GlobalClock/GlobalClock.h"
 
 #include <iostream>
+
+#include <set>
 
 Server::Server()
 	: MAX_NUM_CLIENTS(2), NUM_CHANNELS(1), TIMEOUT_LIMIT_MS(1000)
 	, server(nullptr), MINIMUM_PORT(10000), MAXIMUM_PORT(20000)
 	, eNetEvent()
+	, succesfullyCreated(false), lastTimeTriedCreation(0.0f), RETRY_CREATION_DELTA_TIME(1.0f)
+	, TIME_BETWEEN_PINGS(1.0f), MAXIMUM_TIME_BEFORE_DECLARING_CONNECTION_LOST(5.0f)
+	, lastKnownBoardConfiguration("")
 {
 	this->address.host = ENET_HOST_ANY;
 	this->address.port = 0;
@@ -27,25 +33,139 @@ Server& Server::get()
 void Server::start()
 {
 	this->address.port = RandomGenerator::randomUniformInt(this->MINIMUM_PORT, this->MAXIMUM_PORT);
+	this->succesfullyCreated = false;
+	this->lastTimeTriedCreation = 0.0f;
+	this->connectedClients.clear();
+	this->stop();
+}
 
-	this->server = enet_host_create(&this->address, this->MAX_NUM_CLIENTS, this->NUM_CHANNELS, 0, 0); // 0, 0 inseamna fara limite pe latimea de banda
+void Server::handleReceivedPacket()
+{
+	std::string clientKey = this->getClientKey(this->eNetEvent.peer->address);
 
-	if (this->server == nullptr)
+	if (this->eNetEvent.packet->dataLength == 0)
 	{
-		std::cout << "Error: ENet server host creation failed" << std::endl;
+		std::cout << "Warning: Server received empty packet" << std::endl;
 		return;
 	}
+
+	std::string receivedMessage((char*)this->eNetEvent.packet->data);
+	std::cout << "Received Message: " << receivedMessage << " from " << clientKey << std::endl;
+
+	if (this->connectedClients.size() == this->MAX_NUM_CLIENTS
+		&& this->connectedClients.find(clientKey) == this->connectedClients.end())
+	{
+		std::cout << "Warning: Server reached maximum number of clients and could not allow a recently arrived client to connect" << std::endl;
+		return;
+	}
+
+	if (this->connectedClients.find(clientKey) == this->connectedClients.end())
+	{
+		this->connectedClients.insert({
+			clientKey, ClientData()
+			});
+		this->connectedClients.find(clientKey)->second.peer = this->eNetEvent.peer;
+	}
+	this->connectedClients.find(clientKey)->second.lastTimeReceivedPing = GlobalClock::get().getCurrentTime();
+
+	if (receivedMessage == "white")
+	{
+		this->connectedClients.find(clientKey)->second.color = Color::WHITE;
+	}
+	else if (receivedMessage == "black")
+	{
+		this->connectedClients.find(clientKey)->second.color = Color::BLACK;
+	}
+	else if (receivedMessage == "requestColor")
+	{
+		std::set<Server::Color> availableColors = { Server::Color::WHITE, Server::Color::BLACK };
+		for (const auto& connectedClient : this->connectedClients)
+		{
+			availableColors.erase(connectedClient.second.color);
+		}
+
+		std::string sentMessage;
+		if (availableColors.size() == 2) // Inca nu s-a setat una dintre culori
+		{
+			sentMessage = "none";
+		}
+		else
+		{
+			if ((*availableColors.begin()) == Server::Color::WHITE)
+			{
+				sentMessage = "white";
+			}
+			else
+			{
+				sentMessage = "black";
+			}
+		}
+
+		ENetPacket* packet = enet_packet_create(sentMessage.c_str(), sentMessage.size() + 1, ENET_PACKET_FLAG_RELIABLE);
+		enet_peer_send(this->eNetEvent.peer, 0, packet);
+		this->connectedClients.find(clientKey)->second.lastTimeSentPing = GlobalClock::get().getCurrentTime();
+	}
+	else if (receivedMessage == "requestConfiguration")
+	{
+		ENetPacket* packet = enet_packet_create(this->lastKnownBoardConfiguration.c_str(), this->lastKnownBoardConfiguration.size() + 1, ENET_PACKET_FLAG_RELIABLE);
+		enet_peer_send(this->eNetEvent.peer, 0, packet);
+		this->connectedClients.find(clientKey)->second.lastTimeSentPing = GlobalClock::get().getCurrentTime();
+	}
+	else if (receivedMessage.find("boardConfiguration:") == 0) // Are prefixul "boardConfiguration:"
+	{
+		this->lastKnownBoardConfiguration = receivedMessage.substr(std::string("boardConfiguration:").size()); // Pornim de la lungimea prefixului
+	}
+	else if (receivedMessage == "ping")
+	{
+		this->connectedClients.find(clientKey)->second.recentlyReceivedPing = true;
+	}
+	else
+	{
+		std::cout << "Warning: Server received unrecognized message" << std::endl;
+	}
+
+	enet_packet_destroy(this->eNetEvent.packet);
 }
 
 void Server::update()
 {
+	if (!this->succesfullyCreated)
+	{
+		if (GlobalClock::get().getCurrentTime() - this->lastTimeTriedCreation < this->RETRY_CREATION_DELTA_TIME)
+			return;
+
+		this->lastTimeTriedCreation = GlobalClock::get().getCurrentTime();
+		this->server = enet_host_create(&this->address, this->MAX_NUM_CLIENTS, this->NUM_CHANNELS, 0, 0); // 0, 0 inseamna fara limite pe latimea de banda
+
+		if (this->server == nullptr)
+		{
+			std::cout << "Error: ENet server host creation failed" << std::endl;
+		}
+		else
+		{
+			this->succesfullyCreated = true;
+			std::cout << "Server started on port " << this->address.port << std::endl;
+		}
+
+		return;
+	}
+
+
+	// Vedem ce pachete am primit.
 	// code = 0 inseamna ca nu a fost niciun eveniment
 	int code = enet_host_service(this->server, &this->eNetEvent, this->TIMEOUT_LIMIT_MS);
 	if (code > 0)
 	{
 		switch (this->eNetEvent.type)
 		{
-			// TODO:
+			/*
+			case ENET_EVENT_TYPE_CONNECT:
+				std::cout << "Client connected" << std::endl;
+				break;
+			*/
+			case ENET_EVENT_TYPE_RECEIVE:
+				this->handleReceivedPacket();
+				break;
 			default:
 				std::cout << "Warning: Server received unrecognized event type" << std::endl;
 				break;
@@ -55,11 +175,42 @@ void Server::update()
 	{
 		std::cout << "Error: Server service failed" << std::endl;
 	}
+
+	
+	// Vedem daca am pierdut conexiunea cu cineva.
+	for (auto& connectedClient : this->connectedClients)
+	{
+		if (GlobalClock::get().getCurrentTime() - connectedClient.second.lastTimeReceivedPing > this->MAXIMUM_TIME_BEFORE_DECLARING_CONNECTION_LOST)
+		{
+			connectedClient.second.recentlyReceivedPing = false;
+		}
+	}
+
+	// Apoi trimitem ping-urile catre clienti.
+	for (auto& connectedClient : this->connectedClients)
+	{
+		std::string sentMessage = "ping";
+
+		for (auto& otherConnectedClient : this->connectedClients)
+		{
+			if (connectedClient.first == otherConnectedClient.first)
+				continue;
+
+			if (GlobalClock::get().getCurrentTime() - otherConnectedClient.second.lastTimeReceivedPing > this->MAXIMUM_TIME_BEFORE_DECLARING_CONNECTION_LOST)
+				sentMessage += "0";
+			else
+				sentMessage += "1";
+		}
+
+		ENetPacket* packet = enet_packet_create(sentMessage.c_str(), sentMessage.size() + 1, ENET_PACKET_FLAG_RELIABLE);
+		enet_peer_send(connectedClient.second.peer, 0, packet);
+	}
 }
 
 void Server::stop()
 {
 	if (this->server != nullptr)
 		enet_host_destroy(this->server);
+	this->server = nullptr;
 }
 
--- BoardGameBattle/BoardGameBattle/source/Server/Server.h
+++ BoardGameBattle/BoardGameBattle/source/Server/Server.h
@@ -1,10 +1,35 @@
 #pragma once
 
 #include <enet/enet.h>
+#include <string>
 #include <map>
 
 class Server
 {
+private:
+	enum class Color
+	{
+		WHITE,
+		BLACK,
+		NONE,
+	};
+
+	struct ClientData
+	{
+		float lastTimeReceivedPing;
+		float lastTimeSentPing;
+		Server::Color color;
+		bool recentlyReceivedPing;
+		ENetPeer* peer;
+
+		ClientData()
+			: lastTimeReceivedPing(0.0f), lastTimeSentPing(0.0f), color(Server::Color::NONE)
+			, recentlyReceivedPing(false), peer(nullptr)
+		{
+
+		}
+	};
+
 private:
 	Server();
 	~Server();
@@ -23,6 +48,22 @@ private:
 	const int MAXIMUM_PORT;
 
 	ENetEvent eNetEvent;
+
+	bool succesfullyCreated;
+	float lastTimeTriedCreation;
+	const float RETRY_CREATION_DELTA_TIME;
+
+	const float TIME_BETWEEN_PINGS;
+	const float MAXIMUM_TIME_BEFORE_DECLARING_CONNECTION_LOST;
+
+	std::map<std::string, ClientData> connectedClients;
+
+	std::string lastKnownBoardConfiguration;
+
+	// Atentie aici la unicitatea cheii
+	inline std::string getClientKey(const ENetAddress& address) const { return std::to_string(address.host); }
+
+	void handleReceivedPacket();
 	
 public:
 	static Server& get();
