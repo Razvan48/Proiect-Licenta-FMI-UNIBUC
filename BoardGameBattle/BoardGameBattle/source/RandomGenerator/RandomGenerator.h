2c4efa265621c42cbe1529422615fde8c8cc1c83
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2025-02-08 22:55:46 +0200

    new function for random number generation

--- BoardGameBattle/BoardGameBattle/source/RandomGenerator/RandomGenerator.h
+++ BoardGameBattle/BoardGameBattle/source/RandomGenerator/RandomGenerator.h
@@ -4,4 +4,5 @@ namespace RandomGenerator
 {
 	double randomUniformDouble(double minimum, double maximum);
 	int randomUniformInt(int minimum, int maximum);
+	unsigned long long randomUniformUnsignedLongLong(unsigned long long minimum, unsigned long long maximum);
 };
