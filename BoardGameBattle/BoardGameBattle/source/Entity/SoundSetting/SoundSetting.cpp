af441a2de19d05edd050597c8b7c958030d2dbef
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2025-02-27 00:16:21 +0200

    small improvement UI settings menu

--- BoardGameBattle/BoardGameBattle/source/Entity/SoundSetting/SoundSetting.cpp
+++ BoardGameBattle/BoardGameBattle/source/Entity/SoundSetting/SoundSetting.cpp
@@ -2,10 +2,10 @@
 
 #include "../../AssetManager/AssetManager.h"
 
-SoundSetting::SoundSetting(float centerPosX, float centerPosY, float width, float height, float rotateAngle, const glm::vec3& color, const std::string& fontName, const std::string& text, Button buttonOn, Button buttonOff)
+SoundSetting::SoundSetting(float centerPosX, float centerPosY, float width, float height, float rotateAngle, const glm::vec3& color, const std::string& fontName, const std::string& text, Button buttonOnOff)
 	: Entity(centerPosX, centerPosY, width, height, rotateAngle)
 	, TextEntity(centerPosX, centerPosY, width, height, rotateAngle, color, fontName, text)
-	, buttonOn(buttonOn), buttonOff(buttonOff)
+	, buttonOnOff(buttonOnOff)
 {
 
 }
@@ -18,26 +18,27 @@ SoundSetting::~SoundSetting()
 void SoundSetting::draw()
 {
 	TextEntity::draw();
-	this->buttonOn.draw();
-	this->buttonOff.draw();
+	this->buttonOnOff.draw();
 }
 
 void SoundSetting::update()
 {
 	TextEntity::update();
-	this->buttonOn.update();
-	this->buttonOff.update();
+	this->buttonOnOff.update();
 
-	if (this->buttonOn.getRecentlyInteractedWith())
+	if (this->buttonOnOff.getRecentlyInteractedWith())
 	{
-		AssetManager::get().setSoundEnabled(true);
-		this->text = "Sound: ON";
-		this->color = glm::vec3(0.0f, 1.0f, 0.0f);
-	}
-	else if (this->buttonOff.getRecentlyInteractedWith())
-	{
-		AssetManager::get().setSoundEnabled(false);
-		this->text = "Sound: OFF";
-		this->color = glm::vec3(1.0f, 0.0f, 0.0f);
+		if (AssetManager::get().getSoundEnabled())
+		{
+			AssetManager::get().setSoundEnabled(false);
+			this->text = "Sound: OFF";
+			this->color = glm::vec3(1.0f, 0.0f, 0.0f);
+		}
+		else
+		{
+			AssetManager::get().setSoundEnabled(true);
+			this->text = "Sound: ON";
+			this->color = glm::vec3(0.0f, 1.0f, 0.0f);
+		}
 	}
 }
