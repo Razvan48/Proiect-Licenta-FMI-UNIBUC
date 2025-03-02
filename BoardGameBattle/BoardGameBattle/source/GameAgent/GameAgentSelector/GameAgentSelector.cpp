ffd7caa45668acde67f117fb088feb5341897694
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2025-03-02 18:36:18 +0200

    added boiler plate code for cachedGreedyExpectedMinMaxAgent

--- BoardGameBattle/BoardGameBattle/source/GameAgent/GameAgentSelector/GameAgentSelector.cpp
+++ BoardGameBattle/BoardGameBattle/source/GameAgent/GameAgentSelector/GameAgentSelector.cpp
@@ -1,10 +1,11 @@
 #include "GameAgentSelector.h"
 
 #include "../GreedyExpectedMinMaxAgent/GreedyExpectedMinMaxAgent.h"
+#include "../CachedGreedyExpectedMinMaxAgent/CachedGreedyExpectedMinMaxAgent.h"
 
 GameAgent& GameAgentSelector::get()
 {
-	return GreedyExpectedMinMaxAgent::get();
+	return CachedGreedyExpectedMinMaxAgent::get();
 }
 
 
